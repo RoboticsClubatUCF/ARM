03e26a380cb9312147686533c9461d08c1e2f249
RubenDennis <145509659+RubenDennis@users.noreply.github.com>
2023-11-14 18:43:20 -0500

    Add files via upload

---
+++ joystick.ino
@@ -0,0 +1,30 @@
+/*
+ * Created by ArduinoGetStarted.com
+ *
+ * This example code is in the public domain
+ *
+ * Tutorial page: https://arduinogetstarted.com/tutorials/arduino-joystick
+ */
+
+#define VRX_PIN  A0 // Arduino pin connected to VRX pin
+#define VRY_PIN  A1 // Arduino pin connected to VRY pin
+
+int xValue = 0; // To store value of the X axis
+int yValue = 0; // To store value of the Y axis
+
+void setup() {
+  Serial.begin(9600) ;
+}
+
+void loop() {
+  // read analog X and Y analog values
+  xValue = analogRead(VRX_PIN);
+  yValue = analogRead(VRY_PIN);
+
+  // print data to Serial Monitor on Arduino IDE
+  Serial.print("x = ");
+  Serial.print(xValue);
+  Serial.print(", y = ");
+  Serial.println(yValue);
+  delay(200);
+}
